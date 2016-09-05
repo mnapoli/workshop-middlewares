c73536705f6f86b8aa2c20698780007c91b2c8c0
Matthieu Napoli <matthieu@mnapoli.fr>
2016-09-05 23:36:48 +0200

    First step

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\SapiEmitter;
+use Zend\Diactoros\Response\TextResponse;
+use Zend\Diactoros\ServerRequestFactory;
+
+// Serve static files
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
+    return false;
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$application = function (ServerRequestInterface $request, callable $next) {
+    // Write your application: return a response
+    /**
+     * @see \Psr\Http\Message\ResponseInterface
+     */
+};
+
+// Run the application
+$next = function () {
+    return new TextResponse('Page not found', 404);
+};
+$response = $application(ServerRequestFactory::fromGlobals(), $next);
+
+// Emit the response (with header() and echo)
+(new SapiEmitter)->emit($response);
