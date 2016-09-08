ef32a71e8154a51a468cbaf4879c57f15c1e54b9
Matthieu Napoli <matthieu@mnapoli.fr>
2016-09-08 12:46:03 +0200

    Display all errors

--- web/index.php
+++ web/index.php
@@ -2,6 +2,9 @@
 
 use Zend\Diactoros\Response\SapiEmitter;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Serve static files
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
     return false;
